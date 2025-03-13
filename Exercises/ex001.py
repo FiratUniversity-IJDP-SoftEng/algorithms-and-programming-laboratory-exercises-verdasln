397d70631c8b39390aafd4c3d3251005e2e58a3c
Verda Aslan <153285355+verdasln@users.noreply.github.com>
2025-03-13 11:48:23 +0300

    Update ex001.py

--- Exercises/ex001.py
+++ Exercises/ex001.py
@@ -1,6 +1,10 @@
 # Your Student ID: 220543016
 # Your Name and Surname: Verda Aslan
 
+print("Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\tHow I wonder what you are")
+
+#ya da 
+
 def print_poem():
     poem = """Twinkle, twinkle, little star,
     How I wonder what you are!
@@ -12,3 +16,6 @@ Twinkle, twinkle, little star,
     print(poem)
   
 print_poem()
+
+
+
