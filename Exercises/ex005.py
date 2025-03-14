2fb0c1ddd9bcecedb8cbbe9ebd34c4cb21e12a93
Verda Aslan <153285355+verdasln@users.noreply.github.com>
2025-03-14 14:57:42 +0300

    Update ex005.py

--- Exercises/ex005.py
+++ Exercises/ex005.py
@@ -1,2 +1,8 @@
 # Your Student ID:
 # Your Name and Surname:
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Hi".ljust(10) + last_name) ## ljust(10)  "Hi" kelimesini sola yaslayıp boşluk ekliyoruz.
+print(first_name.rjust(10)) ## rjust(10)  Adı sağa hizalayarak soyadın altına denk gelmesini sağlıyoruz.
