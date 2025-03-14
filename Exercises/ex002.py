610c418fbb31e2ebe96e6e097971d54b0b449869
Verda Aslan <153285355+verdasln@users.noreply.github.com>
2025-03-14 14:22:07 +0300

    Update ex002.py

--- Exercises/ex002.py
+++ Exercises/ex002.py
@@ -1,2 +1,7 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID: 220543016
+# Your Name and Surname: Verda Aslan
+
+import sys
+
+print("Python version:", sys.version)  
+print("Version info:", sys.version_info) 
