c553d8249d5700b7567a69ba5bbe613d0ab13e6c
Verda Aslan <153285355+verdasln@users.noreply.github.com>
2025-03-14 15:08:37 +0300

    Update ex007.py

--- Exercises/ex007.py
+++ Exercises/ex007.py
@@ -1,2 +1,16 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID: 220543016
+# Your Name and Surname: Verda Aslan
+
+text = input("Enter a string: ")
+
+char_count = {}
+
+for char in text:
+    if char in char_count:
+        char_count[char] += 1  # Eğer harf zaten varsa sayıyı artır
+    else:
+        char_count[char] = 1  # Eğer harf yoksa 1 olarak ekle
+
+print("Character frequencies:")
+for char in sorted(char_count):
+    print(f"{char}: {char_count[char]}")
